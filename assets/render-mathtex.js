5b6eac42b36897f98df9222a821bc6dfb65aa76c
Christopher Morton <sonro@gmx.com>
2025-11-24 15:50:34 +0000

    fix: remove temml mathtex

--- assets/render-mathtex.js
+++
@@ -1,8 +0,0 @@
-let eqns = document.querySelectorAll("script[type='math/tex']");
-for (let i=eqns.length-1; i>=0; i--) {
-    let eqn = eqns[i];
-    let src = eqn.text;
-    let d = eqn.closest('p') == null; 
-    eqn.outerHTML = temml.renderToString(src, { displayMode: d });
-}
-
